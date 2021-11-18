a1de8f17195bbe9cc4751864c8bb74c6668d8688
Leandro Moreira <leandromoreiravaz@gmail.com>
2021-11-17 22:23:00 -0300

    1. New colors + Terminus font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font = "Terminus:pixelsize=14:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
@@ -92,45 +92,62 @@ char *termname = "st-256color";
  *	stty tabs
  */
 unsigned int tabspaces = 8;
-
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
-	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+
+	  /* 8 normal colors */
+	[0] = "#090300", /* black   */
+ 	[1] = "#db2d20", /* red     */
+ 	[2] = "#007e3f", /* green   */
+	[3] = "#fded02", /* yellow  */
+ 	[4] = "#0185e4", /* blue    */
+	[5] = "#823c74", /* magenta */
+	[6] = "#23c8c8", /* cyan    */
+	[7] = "#a5a2a2", /* white   */
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	[8]  = "#5c5855", /* black   */
+	[9]  = "#db2d20", /* red     */
+	[10] = "#007e3f", /* green   */
+	[11] = "#fded02", /* yellow  */
+  	[12] = "#0185e4", /* blue    */
+ 	[13] = "#823c74", /* magenta */
+	[14] = "#23c8c8", /* cyan    */
+	[15] = "#f7f7f7", /* white   */
 
-	[255] = 0,
 
-	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
+	/* special colors */
+	[256] = "#03030f", /* background */
+	[257] = "#bbbbbb", /* foreground */
 };
+
+/*
+ *  * Default colors (colorname index)
+ *  * foreground, background, cursor
+ *  */
+unsigned int defaultfg = 257;
+unsigned int defaultbg = 256;
+static unsigned int defaultcs = 257;
+static unsigned int defaultrcs = 256;
+
+/*
+ *  * Colors used, when the specific fg == defaultfg. So in reverse mode this
+ *  * will reverse too. Another logic would only make the simple feature too
+ *  * complex.
+ *  */
+static unsigned int defaultitalic = 7;
+static unsigned int defaultunderline = 7;
+
 
 
 /*
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
-unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+//unsigned int defaultfg = 7;
+//unsigned int defaultbg = 0;
+//static unsigned int defaultcs = 256;
+//static unsigned int defaultrcs = 257;
 
 /*
  * Default shape of cursor
